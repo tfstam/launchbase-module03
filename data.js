213b66741e60685a55bc0f089a34909e274473e0
Tamires Santos <tfs.tamsantos@gmail.com>
2020-05-24 13:37:52 -0300

    Updated projects

---
+++ data.js
@@ -0,0 +1,102 @@
+module.exports = [
+  {
+    id: '1',
+    name: 'Project 1',
+    image: 'https://images.unsplash.com/photo-1590221685403-dbc8489d9329?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80',
+    repository: "http://github.com",
+    shortDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, odio. Officia, architecto.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quod molestias vitae esse libero! Quae, doloribus temporibus inventore aliquam quod, saepe ex consequatur architecto recusandae quisquam id et beatae ipsum, mollitia nihil sint deleniti dolores ut. Incidunt officia distinctio at, mollitia similique voluptatibus ipsa corrupti aliquid tempore sunt perferendis hic voluptatum eaque aspernatur sed! Deleniti molestias unde aliquam minus ex natus consequatur corrupti exercitationem rem inventore sint tempora, totam similique. Aut libero soluta cupiditate aspernatur enim harum ipsa pariatur odit?',
+    tags: [
+      {
+        name: 'HTML'
+      },
+      {
+        name: 'CSS'
+      },
+      {
+        name: 'JavaScript'
+      }
+    ]
+  },
+  {
+    id: '2',
+    name: 'Project 2',
+    image: "https://images.unsplash.com/photo-1574092402543-3f854741bef0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+    repository: "http://github.com",
+    shortDescription: 'Lorem elit. Numquam, odio. Officia, architecto.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quod molestias vitae esse libero! Quae, doloribus temporibus inventore aliquam quod, saepe ex consequatur architecto recusandae quisquam id et beatae ipsum, mollitia nihil sint deleniti dolores ut. Incidunt officia distinctio at, mollitia similique voluptatibus ipsa corrupti aliquid tempore sunt perferendis hic voluptatum eaque aspernatur sed! Deleniti molestias unde aliquam minus ex natus consequatur corrupti exercitationem rem inventore sint tempora, totam similique. Aut libero soluta cupiditate aspernatur enim harum ipsa pariatur odit?',
+    tags: [
+      {
+        name: 'HTML'
+      },
+      {
+        name: 'CSS'
+      }
+    ]
+  },
+  {
+    id: '3',
+    name: 'Project 3',
+    image: "https://images.unsplash.com/photo-1574075284234-c5fcde1bb318?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+    repository: "http://github.com",
+    shortDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, odio. Officia, architecto.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quod molestias vitae esse libero! Quae, doloribus temporibus inventore aliquam quod, saepe ex consequatur architecto recusandae quisquam id et beatae ipsum, mollitia nihil sint deleniti dolores ut. Incidunt officia distinctio at, mollitia similique voluptatibus ipsa corrupti aliquid tempore sunt perferendis hic voluptatum eaque aspernatur sed! Deleniti molestias unde aliquam minus ex natus consequatur corrupti exercitationem rem inventore sint tempora, totam similique. Aut libero soluta cupiditate aspernatur enim harum ipsa pariatur odit?',
+    tags: [
+      {
+        name: 'HTML'
+      },
+      {
+        name: 'CSS'
+      },
+      {
+        name: 'JavaScript'
+      },
+      {
+        name: 'NodeJS'
+      }
+    ]
+  },
+  {
+    id: '4',
+    name: 'Project 4',
+    image: "https://images.unsplash.com/photo-1574090113056-26959d9f0d46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+    repository: "http://github.com",
+    shortDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, odio. Officia, architecto.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quod molestias vitae esse libero! Quae, doloribus temporibus inventore aliquam quod, saepe ex consequatur architecto recusandae quisquam id et beatae ipsum, mollitia nihil sint deleniti dolores ut. Incidunt officia distinctio at, mollitia similique voluptatibus ipsa corrupti aliquid tempore sunt perferendis hic voluptatum eaque aspernatur sed! Deleniti molestias unde aliquam minus ex natus consequatur corrupti exercitationem rem inventore sint tempora, totam similique. Aut libero soluta cupiditate aspernatur enim harum ipsa pariatur odit?',
+    tags: []
+  },
+  {
+    id: '5',
+    name: 'Project 5',
+    image: "https://images.unsplash.com/photo-1574122296117-9e708b28e098?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+    repository: "http://github.com",
+    shortDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, odio.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quod molestias vitae esse libero! Quae, doloribus temporibus inventore aliquam quod, saepe ex consequatur architecto recusandae quisquam id et beatae ipsum, mollitia nihil sint deleniti dolores ut. Incidunt officia distinctio at, mollitia similique voluptatibus ipsa corrupti aliquid tempore sunt perferendis hic voluptatum eaque aspernatur sed! Deleniti molestias unde aliquam minus ex natus consequatur corrupti exercitationem rem inventore sint tempora, totam similique. Aut libero soluta cupiditate aspernatur enim harum ipsa pariatur odit?',
+    tags: []
+  },
+  {
+    id: '6',
+    name: 'Project 6',
+    image: "https://images.unsplash.com/photo-1574106552884-803f62d3ba47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80",
+    repository: "http://github.com",
+    shortDescription: 'Consectetur adipisicing elit. Numquam, odio. Officia, architecto.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quod molestias vitae esse libero! Quae, doloribus temporibus inventore aliquam quod, saepe ex consequatur architecto recusandae quisquam id et beatae ipsum, mollitia nihil sint deleniti dolores ut. Incidunt officia distinctio at, mollitia similique voluptatibus ipsa corrupti aliquid tempore sunt perferendis hic voluptatum eaque aspernatur sed! Deleniti molestias unde aliquam minus ex natus consequatur corrupti exercitationem rem inventore sint tempora, totam similique. Aut libero soluta cupiditate aspernatur enim harum ipsa pariatur odit?',
+    tags: [
+      {
+        name: 'HTML'
+      },
+      {
+        name: 'CSS'
+      },
+      {
+        name: 'JavaScript'
+      },
+      {
+        name: 'NodeJS'
+      },
+      {
+        name: 'ReactJS'
+      }
+    ]
+  }
+]
--- server.js
+++ server.js
@@ -1,8 +1,56 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
+const projects = require('./data')
+
+server.use(express.static('public'))
+
+server.set('view engine', 'njk')
+
+nunjucks.configure('views', {
+  express: server,
+  autoescape: false,
+  noCache: true
+})
 
 server.get('/', function(req, res) {
-  return res.send('Hi')
+  const profileData = {
+    avatar_url: '/images/avatar.jpg',
+    name: 'Tamires Santos',
+    role: 'Rocketseat Student',
+    description: 'Learning to be a full-stack developer. Bootcamp LaunchBase <a href="https://rocketseat.com.br/" target="_blanket">Rocketseat</a>.',
+    links: [
+      {
+        name: 'GitHub',
+        url: 'https://github.com/tfstam'
+      },
+      {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/tamires-santos-230747103/'
+      }
+    ]
+  }
+
+  return res.render('about', { profile: profileData })
+})
+
+server.get('/projects', function(req, res) {
+  return res.render('projects', { projects })
+})
+
+server.get('/project', function(req, res) {
+  const id = req.query.id
+
+  const project = projects.find(function(project) {
+    return project.id == id 
+  })
+
+  if ( !project ) {
+    return res.send('Project not found!')
+  }
+
+  return res.render('project', { project })
 })
 
 server.listen(5000, function() {
